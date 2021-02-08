7232d2d45c2d51ba6604fe51d2e88cedacab0d3f
Lasse Huesmann <Lasse.Huesmann@web.de>
2021-02-08 12:52:06 +0100

    Automatisches erzeugen der Liste mit der Datenbank, löschen einzelner element der Liste/DB

--- src/app/saved/saved.page.ts
+++ src/app/saved/saved.page.ts
@@ -1,7 +1,9 @@
 import { Component, Input, ViewChild } from '@angular/core';
 import { NavigationExtras, ActivatedRoute, Router } from '@angular/router';
+import { NavParams } from '@ionic/angular';
 import { ValueAccessor } from '@ionic/angular/directives/control-value-accessors/value-accessor';
 import { Storage } from '@ionic/storage';
+import { Button } from 'protractor';
 
 
 @Component({
@@ -14,15 +16,19 @@ export class SavedPage {
   idInput: HTMLInputElement;
   //list: HTMLElement;
 
-  lastStorageId:number = 0;
 
   constructor(private route: ActivatedRoute, private router: Router, private storage: Storage) {
     this.route.queryParams.subscribe(params => {
-        this.addSavedData(params, this.lastStorageId++);
+      if(this.getSavedDataById("sId" + params.Windchill) == null){     //Windchill mus noch gerundet werden
+        this.addSavedData(params, "sId" + params.Windchill);
+      }
+
     })
   }
 
-  
+  ngOnInit(){
+    this.createContent();
+  }
 
   addSavedData(data:any, storageId: any){
     this.storage.set(storageId, data)
@@ -34,18 +40,20 @@ export class SavedPage {
     this.getSavedDataById(this.idInput.value);
   }
 
-  getSavedDataById(id:any){
+  getSavedDataById(id:any) : any{
     this.storage.get(id).then((val) => {
+      console.log("Data:");
       console.log(val);
+      return val;
     });
   }
   deleteSavedData(){
-    this.idInput = <HTMLInputElement> document.getElementById("inputId");
-    this.deleteSavedDataById(this.idInput.value);
+    this.storage.clear();
   }
 
   deleteSavedDataById(id:any){
     this.storage.remove(id);
+    this.createContent();
   }
 
   onHomeClicked(){
@@ -54,13 +62,31 @@ export class SavedPage {
 
   createContent(){
     //var list = document.createElement("ion-list");
+    var contentList = document.getElementById("savedList");
+    contentList.innerHTML = "";
+    var list = document.createElement("ion-list");
+    contentList.appendChild(list);
+    
+    /*var listElement = document.createElement("ion-item");
+    var listElementLabel = document.createElement("ion-label");
+    listElementLabel.innerHTML = "test";
+    listElement.appendChild(listElementLabel);
+    list.appendChild(listElement);*/
 
     this.storage.forEach((v,k)=>{
+      var listElement = document.createElement("ion-item");
+      list.appendChild(listElement);
+      const data = new NavParams(v);
+      var itemId = "item" + k;
+      listElement.innerHTML = "<IonItem id=" + itemId + "><IonLabel> Windgeschwindigkeit: " + data.get("Windgeschwindigkeit") + "</IonLabel><IonLabel> Temperatur: " + data.get("Temperatur") + "</IonLabel><IonLabel> Windchill: " + data.get("Windchill") + "</IonLabel></IonItem>";
+      
+      var delButton = document.createElement("ion-button");
+      var listItem = document.getElementById("item" + k);
+      listItem.appendChild(delButton);
+      delButton.addEventListener("click", (event: CustomEvent) => {this.deleteSavedDataById(k);});
       console.log('value',v);
       console.log('key',k);          
     })
-
-
   }
 
   createContentElement(){
